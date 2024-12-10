e063ce7dbc34bf15ad7f66ab7a488bba0bef09ae
muhammadammar24 <muhammadammar2492@gmail.com>
2024-12-10 13:09:07 +0500

    init

--- app/Components/AddNewSection.tsx
+++ app/Components/AddNewSection.tsx
@@ -1,17 +1,21 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function AddNewSection() {
   return (
     <div className="group flex items-center gap-2 justify-center p-1 m-auto max-w-[35em] mt-10 text-center border rounded-[12px] bg-secondary text-accent font-bold cursor-pointer hover:bg-accent hover:text-white transition-all duration-100 ease-in-out">
-      <Image
+      {/* <Image
         src={"/addNewSection.svg"}
         alt="Add New Section"
         width={20}
         height={20}
         priority
         className="w-5 h-5 cursor-pointer group-hover:text-white group-hover:brightness-0 group-hover:invert"
-      />{" "}
+      />{" "} */}
+      <span className="text-[1.2rem] mb-1">+</span>
+      <span>
+
       New Section
+      </span>
     </div>
   );
 }
